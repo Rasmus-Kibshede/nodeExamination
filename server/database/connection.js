4611ebe72b9efaff3bcaccea2cb993ac4aaa8906
Rasmus Kibshede <2740ralle@gmail.com>
2023-01-11 11:48:42 +0100

    added env var for DB name, DB is online now

--- server/database/connection.js
+++ server/database/connection.js
@@ -7,7 +7,7 @@ const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "wizardworld"
+    database: process.env.DB_NAME
 });
 
 
