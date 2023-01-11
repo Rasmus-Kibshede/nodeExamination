54161218eac2008c644026c460a1956b1114bdb1
Rasmus Kibshede <2740ralle@gmail.com>
2023-01-11 14:04:31 +0100

    created connection pool to keep connection to online host

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,12 +3,22 @@ import mysql from "mysql2/promise";
 
 // need to create the database first!
 
-const connection = await mysql.createConnection({
+/* const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+ */
 
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 2,
+    queueLimit: 0
+  });
 
 export default connection;
