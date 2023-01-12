505f9f2773647c73398d9b1d6cb6de65651660e3
Rasmus-Kibshede <2740Ralle@gmail.com>
2023-01-12 12:45:01 +0100

    created wand post routing backend

--- server/routers/WandRouter.js
+++ server/routers/WandRouter.js
@@ -11,6 +11,23 @@ router.get("/wand/:id", async (req, res) => {
     res.send({ wand: rows[0] })
 });
 
+router.post("/wand", async (req, res) => {
+    const wand = req.body;
+    const [rows, _] = await db.execute("INSERT INTO wands(wand_name, wand_core, wand_wood, wand_length) VALUES (?,?,?,?)",
+        [wand.wand_name, wand.wand_core, wand.wand_wood, wand.wand_length]);
+
+    if (rows.affectedRows) {
+        const [result, _] = await db.execute("UPDATE users SET fk_wand_id = ? WHERE user_id = ?",
+            [rows.insertId, wand.user_id]);
+        
+            if(result.affectedRows){
+                
+            }
+    }
+
+    res.send({ message: rows });
+});
+
 
 
 
