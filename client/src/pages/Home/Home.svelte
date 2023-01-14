a1d371212b828192cc27f1798e4793c1c7776871
Rasmus-Kibshede <2740Ralle@gmail.com>
2023-01-14 23:01:59 +0100

    added crests into homepage

--- client/src/pages/Home/Home.svelte
+++ client/src/pages/Home/Home.svelte
@@ -1,10 +1,13 @@
 <script>
     import { jwtToken } from "../../../store/globals";
+    import Crests from "../../components/Crests/Crests.svelte";
 
     console.log("cookie test", $jwtToken);
 </script>
 
 <h1>Welcome to Hogwarts!</h1>
+
+<Crests />
 
 <style>
 </style>
