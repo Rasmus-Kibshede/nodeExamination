7b8199812d5ef3fc511eaba7694fa83bfd89c2c5
Rasmus-Kibshede <2740Ralle@gmail.com>
2023-01-15 12:47:36 +0100

    added validation on email and fileds for signup

--- client/src/components/Signup/Signup.svelte
+++ client/src/components/Signup/Signup.svelte
@@ -17,35 +17,60 @@
             lastname: document.getElementById("signup_lastname").value,
         };
 
-        let response = await fetch(`${$BASE_URL}/signup`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(user),
-        });
+        if (user.email && user.password && user.firstname && user.lastname) {
+            if (!validate) {
+                let response = await fetch(`${$BASE_URL}/signup`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json;charset=utf-8",
+                    },
+                    body: JSON.stringify(user),
+                });
 
-        const result = await response.json();
-        const message = result.message;
+                const result = await response.json();
+                const message = result.message;
 
-        if (response.ok) {
-            const from = ($location.state && $location.state.from) || "/";
-            navigate(from, { replace: true });
+                if (response.ok) {
+                    const from =
+                        ($location.state && $location.state.from) || "/";
+                    navigate(from, { replace: true });
 
-            // @ts-ignore
-            toastr.success("Signed up", message);
+                    // @ts-ignore
+                    toastr.success("Signed up", message);
+                } else {
+                    // @ts-ignore
+                    toastr.error("Error", message);
+                }
+            } else {
+                // @ts-ignore
+                toastr.error("Invalid syntax email");
+            }
         } else {
             // @ts-ignore
-            toastr.error("Error", message);
+            toastr.error("All feilds must be filled");
         }
     }
+
+    // Validate email with regex
+    const regex_mail_validation = new RegExp(
+        "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"
+    );
+
+    let email;
+    $: validate = email ? !regex_mail_validation.test(email) : false;
 </script>
 
 <div>
     <h1>Signup here</h1>
 
     <label for="signup_email">Email:</label>
-    <input type="email" id="signup_email" placeholder="Enter Email" />
+    <input
+        type="email"
+        id="signup_email"
+        bind:value={email}
+        class:invalid={validate}
+        placeholder="Enter Email"
+    />
 
     <label for="signup_password">Password:</label>
     <input type="text" id="signup_password" placeholder="Enter Password" />
@@ -60,6 +85,9 @@
 </div>
 
 <style>
+    .invalid {
+        border: solid 5px red;
+    }
     div {
         display: grid;
         justify-content: center;
@@ -67,4 +95,3 @@
         row-gap: 10px;
     }
 </style>
-
