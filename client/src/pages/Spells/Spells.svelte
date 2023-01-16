2527b8268983c3b987c9baa9345959912d3c4cf8
Rasmus-Kibshede <2740Ralle@gmail.com>
2023-01-16 21:56:09 +0100

    added code to spell page with add and remove, not done

--- client/src/pages/Spells/Spells.svelte
+++ client/src/pages/Spells/Spells.svelte
@@ -1,11 +1,26 @@
 <script>
-    import { BASE_URL, currentURL } from "../../../store/globals";
+    import {
+        BASE_URL,
+        currentURL,
+        jwtToken,
+        global_user,
+    } from "../../../store/globals";
     import { onMount } from "svelte";
 
     // A part of menu page highlighter
     import { useLocation } from "svelte-navigator";
     const location = useLocation();
     $currentURL = $location.pathname;
+
+    let spells = [];
+    let searchedSpell;
+    let user_spells_ids = [];
+
+    $: searchedSpells = searchedSpell ? search : spells;
+
+    $: search = spells.filter((spell) =>
+        spell.spell_name.toLowerCase().includes(searchedSpell)
+    );
 
     async function fecthSpells() {
         const response = await fetch(`${$BASE_URL}/api/spells`);
@@ -15,19 +30,79 @@
         if (response.ok) {
             spells = result.spells;
         }
+
+        if ($jwtToken) {
+            const response = await fetch(
+                `${$BASE_URL}/api/spells/${$global_user.user_id}`
+            );
+
+            const result = await response.json();
+
+            if (response.ok) {
+                user_spells_ids = result.spell_ids;
+                user_spells_ids[0].test = true;
+            }
+        }
+    }
+
+    async function saveSpells(spell_id) {
+        const response = await fetch(`${$BASE_URL}/api/spells`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                Authorization: "Bearer " + $jwtToken,
+            },
+            body: JSON.stringify({
+                user_id: $global_user.user_id,
+                spell_id: spell_id,
+            }),
+        });
+
+        const result = await response.json();
+
+        const message = result.message;
+
+        if (response.ok) {
+            // @ts-ignore
+            toastr.success(message);
+        } else {
+            // @ts-ignore
+            toastr.error(message);
+        }
+    }
+
+    async function deleteSpell(spell_id) {
+        const response = await fetch(`${$BASE_URL}/api/spells/${spell_id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                Authorization: "Bearer " + $jwtToken,
+            },
+            body: JSON.stringify({
+                user_id: $global_user.user_id,
+            }),
+        });
+
+        const result = await response.json();
+
+        const message = result.message;
+
+        if (response.ok) {
+            // @ts-ignore
+            toastr.success(message);
+        } else {
+            // @ts-ignore
+            toastr.error(message);
+        }
     }
 
     onMount(fecthSpells);
 
-    let spells = [];
-
-    let searchedSpell;
-
-    $: search = spells.filter((spell) =>
-        spell.spell_name.toLowerCase().includes(searchedSpell)
-    );
-
-    $: newSpells = searchedSpell ? search : spells;
+    function spellActionHandler(spell_id) {
+        user_spells_ids.some((item) => item.fk_spell_id === spell_id)
+            ? deleteSpell(spell_id)
+            : saveSpells(spell_id);
+    }
 </script>
 
 <h1>Spells</h1>
@@ -38,10 +113,29 @@
     bind:value={searchedSpell}
 />
 
-{#each newSpells as { spell_name, spell_description }}
-    <h4>{spell_name}</h4>
-    <p>{spell_description}</p>
-    <br />
+{#each searchedSpells as spell}
+    <div class="flex_box">
+        <div>
+            <h4>{spell.spell_name}</h4>
+            <p>{spell.spell_description}</p>
+        </div>
+        {#if $jwtToken}
+            <button on:click={() => spellActionHandler(spell.spell_id)}
+                >{user_spells_ids.some(
+                    (item) => item.fk_spell_id === spell.spell_id
+                )
+                    ? "Forget"
+                    : "Learn"}</button
+            >
+        {/if}
+    </div>
 {/each}
 
-<style></style>
+<style>
+    .flex_box {
+        margin-bottom: 50px;
+        padding: 10px;
+        border: solid 1px black;
+        border-radius: 3px;
+    }
+</style>
--- server/routers/SpellRouter.js
+++ server/routers/SpellRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import db from "../database/connection.js"
+import { authenticateToken } from "../util/auth.js";
 // import fs from "fs";
 
 const router = Router();
@@ -13,6 +14,46 @@ router.get("/api/spells", async (req, res) => {
     } else {
         res.status(400).send({ message: "No spells found" });
     }
+});
+
+router.get("/api/spells/:id", async (req, res) => {
+    const [rows, _] = await db.execute("SELECT fk_spell_id FROM users_spells WHERE fk_user_id = ?",
+        [req.params.id]);
+
+    if (rows) {
+        res.status(200).send({ spell_ids: rows });
+    } else {
+        res.status(400).send({ message: "No spells found" });
+    }
+});
+
+router.delete("/api/spells/:id", authenticateToken, async (req, res) => {
+    const [rows, _] = await db.execute("DELETE FROM users_spells WHERE fk_user_id = ? AND fk_spell_id = ?",
+        [req.body.user_id, req.params.id]);
+
+    if (rows) {
+        res.status(200).send({ message: "Spell deleted" });
+    } else {
+        res.status(400).send({ message: "No spells found" });
+    }
+});
+
+
+router.post("/api/spells", authenticateToken, async (req, res) => {
+
+    const spell_id = req.body.spell_id;
+    const user_id = req.body.user_id;
+
+    const [rows, _] = await db.execute("INSERT INTO users_spells (fk_user_id, fk_spell_id) VALUES (?, ?)",
+        [user_id, spell_id]);
+
+    if (rows.affectedRows) {
+        res.status(200).send({ message: "Spell saved" });
+    } else {
+        res.status(400).send({ message: "Spell not saved" });
+    }
+
+
 });
 
 // Get spells from json file
